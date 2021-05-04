87b245d0cf10322322e5434c7502151fb1f34b51
Thomas Bonk <thomas@meandmymac.de>
2021-05-04 18:49:22 +0200

    migration to SPM

---
+++ Sources/NuklearKit/NuklearKit.swift
@@ -0,0 +1,3 @@
+struct NuklearKit {
+    var text = "Hello, World!"
+}
---
+++ Tests/NuklearKitTests/NuklearKitTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import NuklearKit
+
+    final class NuklearKitTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(NuklearKit().text, "Hello, World!")
+        }
+    }
